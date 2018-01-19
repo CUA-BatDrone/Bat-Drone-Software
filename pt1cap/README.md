47ad6a07ad5395d608c921c703c6b0e403e1e3b7
michaellmonaghan <michaellmonaghan@gmail.com>
2018-01-19 02:49:56 -0500

    Changed syntax and handles Ctrl+C

--- pt1cap/README.md
+++ pt1cap/README.md
@@ -1,26 +1,27 @@
 
 # Description
 
-Writes a number of frames captured from the Lepton camera using the Pure Thermal 1 board to a file in binary format.
+Writes frames captured from the Lepton camera using the Pure Thermal 1 board to a file in binary format.
 
 # Usage
 
-`pt1cap <device> <number_of_frames> [filename]`
+`pt1cap <device> [filename] [number_of_frames]``
+device is the path of the pure thermal 1 video device. Typically /dev/video0 or /dev/video1... usually /dev/video1 .
+filename defaults to the current unix timestamp with a bin suffix. e.g. 1510084031.bin
+number_of_frames defaults to 0.
+**Note:** Lepton records at 8.6 frames per second
 
-The default filename is `capture.bin`
+# Examples
+`pt1cap /dev/video1`
+Captures frames to a file named the current unix timestamp until Ctrl+C is pressed.
 
-# Example 
+`pt1cap /dev/video1 capture.bin`
+Captures frames to capture.bin until user presses Ctrl+C
 
-Saves about 10 seconds of video to capture.bin . The Lepton camera records at 8.6 frames per second.
-
-`pt1cap /dev/video1 86 capture.bin`
-
-
-Captures about 10 seconds to a file named "capture" + unix timestamp + ".bin". e.g. cap1510084031.bin
-
-`pt1cap /dev/video1 86 cap$(date +%s).bin`
+`pt1cap /dev/video1 capture.bin 86`
+Captures frames to capture.bin for about 10 seconds or until user presses Ctrl+C
 
 
 # File Format
 
-Each frame is stored one after the other in sequence. A frame is 80x60 pixels. Each pixel is stored as a 16 bit unsigned integer with a maximum value of 0x3FFF (14 bits). 
+Each frame is stored one after the other in sequence. A frame is 80x60 pixels. Each pixel is stored as a 16 bit unsigned integer with a maximum value of 0x3FFF (14 bits).
--- pt1cap/main.c
+++ pt1cap/main.c
@@ -1,21 +1,65 @@
 #include "pt1.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+
+int run = 1;
+
+void handle_sigint(int signum) {
+  run = 0;
+}
 
 int main(int argc, char* argv[]) {
-  if (argc < 3) {
-    printf("Usage: %s <device> <number_of_frames> [filename]\nNote: Lepton records at 8.6 frames per second\n\nExample: %s /dev/video1 86\nCaptures about 10 seconds of video", argv[0], argv[0]);
+  if (argc > 4 || argc < 2) {
+    printf("\
+      Captures frames to filename until Ctrl+C is pressed.\n\
+      \n\
+      Usage: %s <device> [filename] [number_of_frames]\n\
+      \tdevice is the path of the pure thermal 1 video device. Usually /dev/video1 .\n\
+      \tfilename defaults to the current unix timestamp with a bin suffix. \n\
+      \te.g. 1510084031.bin\n\
+      \tnumber_of_frames defaults to 0.\n\
+      \tNote: Lepton records at 8.6 frames per second\n\
+      \n\
+      Example: %s /dev/video1\n\
+      \tCaptures frames to a file named whatever the current unix timestamp is with a \n\
+      \t.bin suffix until Ctrl+C is pressed.\n\
+      \n\
+      Example: %s /dev/video1 capture.bin\n\
+      \tCaptures frames to capture.bin until user presses Ctrl+C\n\
+      \n\
+      Example: %s /dev/video1 capture.bin 86\n\
+      \tCaptures frames to capture.bin for about 10 seconds or until user presses Ctrl+C\n\
+      \n\
+    ", argv[0], argv[0], argv[0], argv[0]);
     return -1;
   }
+
+  sigaction(SIGINT, &((struct sigaction) {.sa_handler = &handle_sigint}), NULL);
+
   char* device_name = argv[1];
-  int num_frames = atoi(argv[2]);
-  char* filename = argc < 4 ? "capture.bin" : argv[3];
+  char* filename;
+  int num_frames;
+
+  if (argc > 2) {
+    filename = argv[2];
+  } else {
+    filename = (char*) ((char[24]) {});
+    sprintf(filename, "%llu.bin", (long long unsigned int) time(NULL));
+  }
+  if (argc > 3) {
+    num_frames = atoi(argv[3]);
+  } else {
+    num_frames = 0;
+  }
+
   pt1_init(device_name);
   FILE *fd = fopen(filename, "w");
 
   pt1_start();
   struct frame frame;
-  for (int i = 0; i < num_frames || num_frames == 0; i++) {
+  for (int i = 0; i < num_frames || num_frames == 0 && run; i++) {
     pt1_get_frame(&frame);
     fwrite(frame.start, 1, frame.length, fd);
   }
