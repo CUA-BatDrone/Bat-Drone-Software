b678b5a2a09a9842931d2878c99a8a8eae966d1a
Michael Monaghan <michaellmonaghan@gmail.com>
2018-03-29 03:48:49 -0400

    Joystick support

--- gse/main.cpp
+++ gse/main.cpp
@@ -9,6 +9,18 @@
 
 using namespace std;
 
+SDL_Joystick *getJoystick() {
+  for (int i = 0; i < SDL_NumJoysticks(); ++i) {
+    SDL_Joystick *joystick = SDL_JoystickOpen(i);
+    if (joystick) {
+      cout << "Using " << SDL_JoystickName(joystick) << endl;
+      return joystick;
+    }
+  }
+  cout << "Using keyboard" << endl;
+  return NULL;
+}
+
 int main(int argc, char* argv[]) {
   try {
     const char *address = "127.0.0.1";
@@ -33,10 +45,11 @@ int main(int argc, char* argv[]) {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
-    // SDL_Event event;
+    SDL_Joystick *joystick = NULL;
+    int last_num_joysticks = 0;
 
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0) {
       SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
       return 3;
     }
@@ -56,6 +69,14 @@ int main(int argc, char* argv[]) {
     bool run = true;
     float last_thrust = -1;
     while (run) {
+
+      // Setup game controllers
+      int num_joysticks = SDL_NumJoysticks();
+      if (num_joysticks != last_num_joysticks) {
+        last_num_joysticks = num_joysticks;
+        joystick = getJoystick();
+      }
+
       // check for events
       SDL_Event e;
       while (SDL_PollEvent(&e)) {
@@ -68,105 +89,112 @@ int main(int argc, char* argv[]) {
       ControlPacketElement c;
       float rate = 1;
       // Direction inputted with WASDQE or arrowkeys and page up/down.
-      if (keyboard[SDL_SCANCODE_RETURN]) {
+      if (joystick) {
+        c.thrust = SDL_JoystickGetAxis(joystick, 3) / (float)-0x7FFF;
+        c.pitch = SDL_JoystickGetAxis(joystick, 1) / (float) -0x7FFF;
+        c.roll = SDL_JoystickGetAxis(joystick, 0) / (float) 0x7FFF;
+        c.yaw = SDL_JoystickGetAxis(joystick, 2) / (float) 0x7FFF;
+        last_thrust = -1.0;
+      } else {
+        if (keyboard[SDL_SCANCODE_RETURN]) {
         c.pitch = -1;
         c.yaw = 1;
         c.roll = -1;
         c.thrust = -1;
         last_thrust = -1;
-      } else {
-        // Forward
-        if (keyboard[SDL_SCANCODE_W] || keyboard[SDL_SCANCODE_UP]) {
-          // And back
-          if (keyboard[SDL_SCANCODE_S] || keyboard[SDL_SCANCODE_DOWN]) {
+        } else {
+          // Forward
+          if (keyboard[SDL_SCANCODE_W] || keyboard[SDL_SCANCODE_UP]) {
+            // And back
+            if (keyboard[SDL_SCANCODE_S] || keyboard[SDL_SCANCODE_DOWN]) {
+              c.pitch = 0;
+            // Just forward
+            } else {
+              c.pitch = rate;
+            }
+          // Backward
+          } else if (keyboard[SDL_SCANCODE_S] || keyboard[SDL_SCANCODE_DOWN]) {
+            c.pitch = -rate;
+          // None
+          } else {
             c.pitch = 0;
-          // Just forward
-          } else {
-            c.pitch = rate;
           }
-        // Backward
-        } else if (keyboard[SDL_SCANCODE_S] || keyboard[SDL_SCANCODE_DOWN]) {
-          c.pitch = -rate;
-        // None
-        } else {
-          c.pitch = 0;
-        }
-        // Left
-        if (keyboard[SDL_SCANCODE_A] || keyboard[SDL_SCANCODE_LEFT]) {
-          // And Right
-          if (keyboard[SDL_SCANCODE_D] || keyboard[SDL_SCANCODE_RIGHT]) {
+          // Left
+          if (keyboard[SDL_SCANCODE_A] || keyboard[SDL_SCANCODE_LEFT]) {
+            // And Right
+            if (keyboard[SDL_SCANCODE_D] || keyboard[SDL_SCANCODE_RIGHT]) {
+              c.roll = 0;
+            // Just left
+            } else {
+              c.roll = -rate;
+            }
+          // Right
+          } else if (keyboard[SDL_SCANCODE_D] || keyboard[SDL_SCANCODE_RIGHT]) {
+            c.roll = rate;
+          // None
+          } else {
             c.roll = 0;
-          // Just left
-          } else {
-            c.roll = -rate;
           }
-        // Right
-        } else if (keyboard[SDL_SCANCODE_D] || keyboard[SDL_SCANCODE_RIGHT]) {
-          c.roll = rate;
-        // None
-        } else {
-          c.roll = 0;
-        }
-        // Rotate Left
-        if (keyboard[SDL_SCANCODE_Q] || keyboard[SDL_SCANCODE_PAGEUP]) {
-          // And right
-          if (keyboard[SDL_SCANCODE_E] || keyboard[SDL_SCANCODE_PAGEDOWN]) {
+          // Rotate Left
+          if (keyboard[SDL_SCANCODE_Q] || keyboard[SDL_SCANCODE_PAGEUP]) {
+            // And right
+            if (keyboard[SDL_SCANCODE_E] || keyboard[SDL_SCANCODE_PAGEDOWN]) {
+              c.yaw = 0;
+            // Just left
+            } else {
+              c.yaw = -rate;
+            }
+          // Rotate Right
+          } else if (keyboard[SDL_SCANCODE_E] || keyboard[SDL_SCANCODE_PAGEDOWN]) {
+            c.yaw = rate;
+          } else {
             c.yaw = 0;
-          // Just left
-          } else {
-            c.yaw = -rate;
           }
-        // Rotate Right
-        } else if (keyboard[SDL_SCANCODE_E] || keyboard[SDL_SCANCODE_PAGEDOWN]) {
-          c.yaw = rate;
-        } else {
-          c.yaw = 0;
-        }
 
-        // Thrust inputted using keys '`' through '0' are -1 through 1.
-        // ### Key 0 IS MAX THRUST!!! ###
-        if (keyboard[SDL_SCANCODE_ESCAPE] || keyboard[SDL_SCANCODE_GRAVE]) {
-          last_thrust = -1.0;
-        } else if (keyboard[SDL_SCANCODE_1]) {
-          last_thrust = -0.8;
-        } else if (keyboard[SDL_SCANCODE_2]) {
-          last_thrust = -0.6;
-        } else if (keyboard[SDL_SCANCODE_3]) {
-          last_thrust = -0.4;
-        } else if (keyboard[SDL_SCANCODE_4]) {
-          last_thrust = -0.2;
-        } else if (keyboard[SDL_SCANCODE_5]) {
-          last_thrust = 0.0;
-        } else if (keyboard[SDL_SCANCODE_6]) {
-          last_thrust = 0.2;
-        } else if (keyboard[SDL_SCANCODE_7]) {
-          last_thrust = 0.4;
-        } else if (keyboard[SDL_SCANCODE_8]) {
-          last_thrust = 0.6;
-        } else if (keyboard[SDL_SCANCODE_9]) {
-          last_thrust = 0.8;
-        } else if (keyboard[SDL_SCANCODE_0]) {
-          last_thrust = 1.0;
-        }
-        if (keyboard[SDL_SCANCODE_SPACE]) {
-          if (last_thrust <= 0.9 && !keyboard[SDL_SCANCODE_LCTRL]) {
-            c.thrust = last_thrust + 0.1;
+          // Thrust inputted using keys '`' through '0' are -1 through 1.
+          // ### Key 0 IS MAX THRUST!!! ###
+          if (keyboard[SDL_SCANCODE_ESCAPE] || keyboard[SDL_SCANCODE_GRAVE]) {
+            last_thrust = -1.0;
+          } else if (keyboard[SDL_SCANCODE_1]) {
+            last_thrust = -0.8;
+          } else if (keyboard[SDL_SCANCODE_2]) {
+            last_thrust = -0.6;
+          } else if (keyboard[SDL_SCANCODE_3]) {
+            last_thrust = -0.4;
+          } else if (keyboard[SDL_SCANCODE_4]) {
+            last_thrust = -0.2;
+          } else if (keyboard[SDL_SCANCODE_5]) {
+            last_thrust = 0.0;
+          } else if (keyboard[SDL_SCANCODE_6]) {
+            last_thrust = 0.2;
+          } else if (keyboard[SDL_SCANCODE_7]) {
+            last_thrust = 0.4;
+          } else if (keyboard[SDL_SCANCODE_8]) {
+            last_thrust = 0.6;
+          } else if (keyboard[SDL_SCANCODE_9]) {
+            last_thrust = 0.8;
+          } else if (keyboard[SDL_SCANCODE_0]) {
+            last_thrust = 1.0;
+          }
+          if (keyboard[SDL_SCANCODE_SPACE]) {
+            if (last_thrust <= 0.9 && !keyboard[SDL_SCANCODE_LCTRL]) {
+              c.thrust = last_thrust + 0.1;
+            } else {
+              c.thrust = last_thrust;
+            }
+          } else if (keyboard[SDL_SCANCODE_LCTRL]) {
+            if (last_thrust >= -0.9) {
+              c.thrust = last_thrust - 0.1;
+            } else {
+              c.thrust = last_thrust;
+            }
           } else {
             c.thrust = last_thrust;
           }
-        } else if (keyboard[SDL_SCANCODE_LCTRL]) {
-          if (last_thrust >= -0.9) {
-            c.thrust = last_thrust - 0.1;
-          } else {
-            c.thrust = last_thrust;
-          }
-        } else {
-          c.thrust = last_thrust;
         }
-
       }
       cmdtlm.control(&c);
-      cout << c.toString();
+      // cout << c.toString();
 
       unsigned char *pixels;
       int pixel_pitch;
@@ -184,6 +212,9 @@ int main(int argc, char* argv[]) {
 
     // SDL_Delay(3000);
 
+    if (joystick) {
+      SDL_JoystickClose(joystick);
+    }
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
