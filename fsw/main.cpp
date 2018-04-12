970c3f35d14e1878a971075c3db0e2006e203909
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-12 04:56:08 -0400

    Renamed port variables

--- fsw/main.cpp
+++ fsw/main.cpp
@@ -10,13 +10,15 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
   const char *address = "232.232.232.232";
-  int port = 2121;
+  int port_send = 2121, port_recv = 1212;
   switch (argc) {
   default:
+  case 4:
+  	port_recv = stoi(argv[3]);
   case 3:
-  	port = stoi(argv[2]);
+  	port_send = stoi(argv[2]);
   case 2:
-	address = argv[1];
+	  address = argv[1];
   case 1:
   case 0:;
   }
@@ -28,13 +30,13 @@ int main(int argc, char* argv[]) {
     cout << "Creating read socket" << endl;
     UDPSocket rs;
     cout << "Binding read socket" << endl;
-    rs.bind(1212);
+    rs.bind(port_recv);
     cout << "Creating write socket" << endl;
     UDPSocket ws;
     cout << "Setting write MTU" << endl;
     ws.setMTUDiscovery(UDPSocket::IP_PMTUDISC_ENUM::DONT);
     UDPPacketReader r(rs);
-    UDPAddrPacketWriter w(Socket::stringToAddr(address, port), ws);
+    UDPAddrPacketWriter w(Socket::stringToAddr(address, port_send), ws);
     CmdTlm cmdtlm(&r, &w);
 
     bool run = true;
