51e7a619cac442bc4fadcd1297daff65f475ac98
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-22 17:12:36 -0400

    Added control.hpp

---
+++ fsw/control.hpp
@@ -0,0 +1,14 @@
+#ifndef CONTROL
+#define CONTROL
+
+/**
+ * A directional control for the drone
+ */
+class Control {
+  float ailerons;
+  float elevator;
+  float thrust;
+  float rudder;
+};
+
+#endif
