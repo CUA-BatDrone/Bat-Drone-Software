aeeb903a6cd77d19f7da8d04937fb1e07d3a12a4
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-23 07:56:55 -0400

    More robust lwir

--- fsw/frame_grabber.cpp
+++ fsw/frame_grabber.cpp
@@ -9,10 +9,17 @@ FrameGrabber::FrameGrabber(Autonomy & autonomy, Sender & sender, const char * de
 
 void FrameGrabber::mainLoop(bool & run) {
   while (run) {
-    if ((pt1_init(device)  < 0 || pt1_deinit) && (pt1_init("/dev/video0")  < 0 || pt1_deinit()) && (pt1_init("/dev/video0")  < 0 || pt1_deinit())) {
-      cerr << "Unable to init libpt1" << endl;
-      this_thread::sleep_for(chrono::seconds(1));
-      continue;
+    if (pt1_init(device) < 0) {
+      pt1_deinit();
+      if (pt1_init("/dev/video0") < 0) {
+        pt1_deinit();
+        if (pt1_init("/dev/video1") < 0) {
+          pt1_deinit();
+          cerr << "Unable to init libpt1 on " << device << endl;
+          this_thread::sleep_for(chrono::seconds(1));
+          continue;
+        }
+      }
     }
     cout << "LWIR Initialized" << endl;
     this_thread::sleep_for(chrono::seconds(2));
