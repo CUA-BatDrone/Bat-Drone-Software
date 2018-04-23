6d23fa3586b6a7d89327457362d895048af8ea36
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-23 05:40:49 -0400

    A very long night

--- fsw/receiver.cpp
+++ fsw/receiver.cpp
@@ -1,2 +1,21 @@
 #include "receiver.hpp"
+#include "commands.hpp"
 
+
+Receiver::Receiver(CmdTlm & cmdtlm, ControlArbiter & controlArbiter) : cmdtlm(cmdtlm), listener(controlArbiter) {}
+
+Receiver::Listener::Listener(ControlArbiter & controlArbiter) : controlArbiter(controlArbiter) {}
+
+void Receiver::Listener::control(float aileron, float elevator, float thrust, float rudder) {
+  Control c;
+  c.aileron = aileron;
+  c.elevator = elevator;
+  c.thrust = thrust;
+  c.rudder = rudder;
+  controlArbiter.manualControl(c);
+}
+void Receiver::mainLoop(bool & run) {
+  while (run) {
+    cmdtlm.telemetry(listener);
+  }
+}
--- fsw/receiver.hpp
+++ fsw/receiver.hpp
@@ -2,12 +2,22 @@
 #define RECEIVER_HPP
 
 #include <cmd_tlm.hpp>
+#include <commands.hpp>
 #include "control_arbiter.hpp"
 
 class Receiver {
 protected:
-  const ControlArbiter & controlArbiter;
-  const CmdTlm & cmdtlm;
+  class Listener : public Commands {
+  protected:
+    ControlArbiter & controlArbiter;
+    // Control the drone
+    virtual void control(float roll, float pitch, float thrust, float yaw);
+    // Switch to tracking mode
+    virtual void track();
+  public:
+    Listener(ControlArbiter & controlArbiter);
+  } listener;
+  CmdTlm & cmdtlm;
 public:
   Receiver(CmdTlm & cmdtlm, ControlArbiter & controlArbiter);
   void mainLoop(bool & run);
