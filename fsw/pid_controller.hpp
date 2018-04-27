4c778bcf2008b4096bdb727408a6fde0889e289a
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-27 17:28:29 -0400

    Error sanity check

--- fsw/pid_controller.hpp
+++ fsw/pid_controller.hpp
@@ -6,7 +6,7 @@ public:
   PIDController(T p = 0, T i = 0, T d = 0, T processMax = 1 , T lastError = 0, T sum = 0, T sumMax = 1) : p(p), i(i), d(d), processMax(processMax), lastError(lastError), sum(sum), sumMax(sumMax) {};
   T process(T error, T time = 1) {
     T r = p * error;
-    errorAvg = errorAvg * 0.8f + error * 0.2f;
+    errorAvg = error;// errorAvg * 0.8f + error * 0.2f;
     r += d * (lastError - errorAvg) / time;
     lastError = errorAvg;
     sum += i * error * time;
