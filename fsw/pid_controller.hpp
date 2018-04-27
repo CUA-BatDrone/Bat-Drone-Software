de5aff12f449629be7e8b1808ae9f714fe0edc53
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-27 16:49:46 -0400

    Added max

--- fsw/pid_controller.hpp
+++ fsw/pid_controller.hpp
@@ -2,8 +2,8 @@
 template <typename T>
 class PIDController {
 public:
-  T p, i, d, sum, sumMax, lastError;
-  PIDController(T p = 0, T i = 0, T d = 0, T lastError = 0, T sum = 0, T sumMax = 1) : p(p), i(i), d(d), lastError(lastError), sum(sum), sumMax(sumMax) {};
+  T p, i, d, processMax, sum, sumMax, lastError;
+  PIDController(T p = 0, T i = 0, T d = 0, T processMax = 1 , T lastError = 0, T sum = 0, T sumMax = 1) : p(p), i(i), d(d), processMax(processMax), lastError(lastError), sum(sum), sumMax(sumMax) {};
   T process(T error, T time = 1) {
     T r = p * error;
     r += d * (lastError - error) / time;
@@ -12,6 +12,8 @@ public:
     if (sum > sumMax) sum = sumMax;
     else if (sum < -sumMax) sum = -sumMax;
     r += sum;
+    if (r > processMax) r = processMax;
+    else if (r < -processMax) r = -processMax;
     return r;
   }
 };
