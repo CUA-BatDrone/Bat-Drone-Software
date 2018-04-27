80f7667c56196441592fb0a25d13c959679b8ded
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-27 17:37:10 -0400

    Adjusting errorAvg gain

--- fsw/pid_controller.hpp
+++ fsw/pid_controller.hpp
@@ -6,7 +6,7 @@ public:
   PIDController(T p = 0, T i = 0, T d = 0, T processMax = 1 , T lastError = 0, T sum = 0, T sumMax = 1) : p(p), i(i), d(d), processMax(processMax), lastError(lastError), sum(sum), sumMax(sumMax), errorAvg(lastError) {};
   T process(T error, T time = 1) {
     T r = p * error;
-    errorAvg = error;// errorAvg * 0.8f + error * 0.2f;
+    errorAvg = errorAvg * 0.9f + error * 0.1f;
     r += d * (lastError - errorAvg) / time;
     lastError = errorAvg;
     sum += i * error * time;
