754e93c12df4c048fbcce5c4af6433a29f5fb390
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-26 20:03:05 -0400

    FSW listens to manual and autonomous commands

--- fsw/receiver.cpp
+++ fsw/receiver.cpp
@@ -1,25 +1,30 @@
 #include "receiver.hpp"
 #include "commands.hpp"
 
-
-Receiver::Receiver(CmdTlm & cmdtlm, ControlArbiter & controlArbiter) : cmdtlm(cmdtlm), listener(controlArbiter) {}
-
-Receiver::Listener::Listener(ControlArbiter & controlArbiter) : controlArbiter(controlArbiter) {}
-
-void Receiver::Listener::control(float aileron, float elevator, float thrust, float rudder) {
-  controlArbiter.setState(ControlArbiter::State::MANUAL);
-  Control c;
-  c.aileron = aileron;
-  c.elevator = elevator;
-  c.thrust = thrust;
-  c.rudder = rudder;
-  controlArbiter.manualControl(c);
-}
-
-void Receiver::Listener::track() {
-  controlArbiter.setState(ControlArbiter::State::AUTONOMOUS);
-}
 void Receiver::mainLoop(bool & run) {
+  class Listener : public Commands {
+  protected:
+    ControlArbiter & controlArbiter;
+  public:
+    // Control the drone
+    virtual void control(float aileron, float elevator, float thrust, float rudder) {
+      Control c;
+      c.aileron = aileron;
+      c.elevator = elevator;
+      c.thrust = thrust;
+      c.rudder = rudder;
+      controlArbiter.manualControl(c);
+    }
+    // Switch to remote control mode
+    virtual void manual() {
+      controlArbiter.setState(ControlArbiter::State::MANUAL);
+    }
+    // Switch to tracking mode
+    virtual void autonomous() {
+      controlArbiter.setState(ControlArbiter::State::AUTONOMOUS);
+    }
+    Listener(ControlArbiter & controlArbiter);
+  } listener (controlArbiter);
   while (run) {
     cmdtlm.telemetry(listener);
   }
