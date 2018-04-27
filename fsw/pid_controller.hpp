cdfbe5a7d0a12ce7ad8c0463e889c233699959fb
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-27 18:47:14 -0400

    Adjust max

--- fsw/pid_controller.hpp
+++ fsw/pid_controller.hpp
@@ -3,7 +3,7 @@ template <typename T>
 class PIDController {
 public:
   T p, i, d, processMax, sum, sumMax, lastError, errorAvg;
-  PIDController(T p = 0, T i = 0, T d = 0, T processMax = 1 , T lastError = 0, T sum = 0, T sumMax = 1) : p(p), i(i), d(d), processMax(processMax), lastError(lastError), sum(sum), sumMax(sumMax), errorAvg(lastError) {};
+  PIDController(T p = 0, T i = 0, T d = 0, T processMax = 0.5, T lastError = 0, T sum = 0, T sumMax = 1) : p(p), i(i), d(d), processMax(processMax), lastError(lastError), sum(sum), sumMax(sumMax), errorAvg(lastError) {};
   T process(T error, T time = 1) {
     T r = p * error;
     errorAvg = errorAvg * 0.9f + error * 0.1f;
