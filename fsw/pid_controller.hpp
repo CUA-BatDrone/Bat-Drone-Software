d55c657962257df189be54bed424e00e37fbd2d1
Michael Monaghan <michaellmonaghan@gmail.com>
2018-04-27 17:12:20 -0400

    Using running average for derivative

--- fsw/pid_controller.hpp
+++ fsw/pid_controller.hpp
@@ -2,12 +2,13 @@
 template <typename T>
 class PIDController {
 public:
-  T p, i, d, processMax, sum, sumMax, lastError;
+  T p, i, d, processMax, sum, sumMax, lastError, errorAvg;
   PIDController(T p = 0, T i = 0, T d = 0, T processMax = 1 , T lastError = 0, T sum = 0, T sumMax = 1) : p(p), i(i), d(d), processMax(processMax), lastError(lastError), sum(sum), sumMax(sumMax) {};
   T process(T error, T time = 1) {
     T r = p * error;
-    r += d * (lastError - error) / time;
-    lastError = error;
+    errorAvg = errorAvg * 0.8f + error * 0.2f;
+    r += d * (lastError - errorAvg) / time;
+    lastError = errorAvg;
     sum += i * error * time;
     if (sum > sumMax) sum = sumMax;
     else if (sum < -sumMax) sum = -sumMax;
